a66ffda8a39161766a528d7fb0ed4b2f9b11b2bf
LPinkham123 <ljpinkham123@gmail.com>
2025-04-22 13:32:36 -0400

    Final CNC Code
    After final project viewed - Using for report

--- Mech1070_final_project_code.m
+++ Mech1070_final_project_code.m
@@ -3,30 +3,33 @@ close all
 % s = CNC_Emulator;
 % set(s, 'EnableTrace');
 
-s = serial('COM4')
+s = serial('COM7')
 set(s,'BaudRate',115200)
 
 fopen(s);
+
+ 
 
 pause(2);
 
 % Cube center coordinates
 cube_coords = [
-    -1.545, -0.75;     % Top Row - Third
-    1.545, -0.75;      % Top Row - Second
-    4.635, -0.75;      % Top Row - Right most
+    1.545, -0.22;     % Top Row - Third
+    -1.545, -0.22;      % Top Row - Second
+    -4.635, -0.22;      % Top Row - Right most
     %-4.635, -0.75;     % Top Row - Left most
-    4.635, -3.5;       % Middle Row - Right
-    4.635, -6.75;       % Bottom Row - Right
+    -4.635, -3.48;       % Middle Row - Right
+    -4.635, -6.73;       % Bottom Row - Right
 ];
+
 
 % Cube destination coordinates
 dest_coords = [
-    -2.0, -3.0;
-    -1.0, -3.0;
-    0.0, -3.0;
-    2, -3; 
-    2, -3;
+    -2.0, -3.0; % Line 1
+    -1.0, -3.0; % Line 2
+    0.0, -3.0;  % Line 3
+    2, -3;      % Stack 1
+    2, -3;      % Stack 2
 ];
 
 fprintf(s,'G17 G20 G90 G94 G54');
@@ -35,40 +38,41 @@ fprintf(s,'G17 G20 G90 G94 G54');
 for i = 1:size(cube_coords, 1)
     % Path for first four cubes on base plate
     if(i <= 4)
-        fprintf(s, 'G1 z1.5 f10'); % Up to miss cubes 
+        fprintf(s, 'G1 z-1.5 f10'); % Up to miss cubes 
         pause(6);
         fprintf(s, ['G1 x', num2str(cube_coords(i, 2)), ' y', num2str(cube_coords(i, 1)), ' f10']); % Goes to cube
         pause(6);
-        fprintf(s, 'G1 z0 f10'); % Drops to cube for pick up
+        fprintf(s, 'G1 z-0 f10'); % Drops to cube for pick up
         pause(); % Pauses until Keypress -- Servo Lift
-        fprintf(s, 'G1 z1.5 f10'); % Lifts to move cube
+        fprintf(s, 'G1 z-1.5 f10'); % Lifts to move cube
         pause(6);
         fprintf(s, ['G1 x', num2str(dest_coords(i, 2)), ' y', num2str(dest_coords(i, 1)), ' f10']); % Goes to cube destination
         pause(6);
-        fprintf(s, 'G1 z0 f10'); % Drops to cube
+        fprintf(s, 'G1 z-0 f10'); % Drops to cube
         pause(); % Pauses until Keypress -- Servo Dropf
     end 
 
     % Path for stacked cube 
     if(i == 5)
-        fprintf(s, 'G1 z1.5 f10'); % Up to miss cubes
+        fprintf(s, 'G1 z-1.5 f10'); % Up to miss cubes
         pause(6);
         fprintf(s, ['G1 x', num2str(cube_coords(i, 2)), ' y', num2str(cube_coords(i, 1)), ' f10']); % Goes to last cube
         pause(6); 
-        fprintf(s, 'G1 z0.5 f10'); % Drops to last cube for pick up
+        fprintf(s, 'G1 z-0.5 f10'); % Drops to last cube for pick up
         pause(); % Pauses until Keypress -- Servo Lift
-        fprintf(s, 'G1 z2.5 f10'); % Lifts to move cube
+        fprintf(s, 'G1 z-2.5 f10'); % Lifts to move cube
         pause(6);
         fprintf(s, ['G1 x', num2str(dest_coords(i, 2)), ' y', num2str(dest_coords(i, 1)), ' f10']); % Goes to last cube destination
         pause(6);
-        fprintf(s, 'G1 z1 f10'); % Down to drop stacked cube
+        fprintf(s, 'G1 z-1 f10'); % Down to drop stacked cube
         pause(); % Pauses until Keypress -- Servo Drop
     end
+
 
     pause(6);
 end
 
-fprintf(s, 'G1 z3.0 f10');
+fprintf(s, 'G1 z-3.0 f10');
 pause(6);
 fprintf(s, 'G1 x0.0, y0.0 f10');
 pause(6);
